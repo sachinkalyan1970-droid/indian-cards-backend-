398cea3c123b1d519572c5ab6da656c6511e808f
sachinkalyan1970-droid <sachinkalyan1970@gmail.com>
2025-08-05 13:56:07 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is running.');
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
